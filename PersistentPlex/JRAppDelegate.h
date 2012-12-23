79693d8eb6298253d405146e0cca50088907e435
James Reuss <James@Dexter-the-MacBook-Pro.local>
2012-12-23 17:12:16 +0000

    Initial Commit

---
+++ PersistentPlex/JRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JRAppDelegate.h
+//  PersistentPlex
+//
+//  Created by James Reuss on 23/12/2012.
+//  Copyright (c) 2012 James Reuss. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface JRAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
